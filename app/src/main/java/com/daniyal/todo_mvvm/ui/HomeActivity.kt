7ff4fced5c320023ab99ca1a96a4df57481a93d0
Daniyal Ahmed <daniyal.khan@tplcorp.com>
2021-05-01 00:15:29 +0500

    - Structure setup and dependency add up - Alpha designs ready - 01/05/2021

---
+++ app/src/main/java/com/daniyal/todo_mvvm/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.daniyal.todo_mvvm.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.daniyal.todo_mvvm.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/daniyal/todo_mvvm/ui/LoginActivity.kt
+++ app/src/main/java/com/daniyal/todo_mvvm/ui/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.daniyal.todo_mvvm
+package com.daniyal.todo_mvvm.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.daniyal.todo_mvvm.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
